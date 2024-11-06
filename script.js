8ea4e270ea6a23ba247e21e86269c5e2ff134103
tauheed0110 <tauheed0110@gmail.com>
2024-11-06 10:22:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const displayTimer = document.getElementById("timer");
+
+let currentDate = new Date();
+
+setInterval(function(){
+    // get the current date
+    currentDate = new Date();
+    // update date in a second
+    displayTimer.textContent = currentDate.toLocaleString();
+}, 1000);
